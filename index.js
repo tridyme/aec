e01c17d24ea7a39aeed3b8067a04f594f0e52c29
triazur <guillaume.cassin@triazur.com>
2021-01-11 13:01:37 +0100

    feat: add wind load at Eurocode 1

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const SectionGeometry = require('./calculations/SectionGeometry');
 const SteelCalculation = require('./calculations/SteelCalculation');
 const WaveCalculation = require('./calculations/WaveCalculation');
+const WindCalculation = require('./calculations/WindCalculation');
+
 
 module.exports = {
   SectionGeometry,
   SteelCalculation,
-  WaveCalculation
+  WaveCalculation,
+  WindCalculation
 };
