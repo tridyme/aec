274450da3253e75376208aa964e9cfbcc344432e
ahmed_abid <ahmed.abid@triazur.com>
2021-03-26 11:05:46 +0100

    SaveSplicePlate.

--- calculations/SteelCalculation/Eurocode3/index.js
+++ calculations/SteelCalculation/Eurocode3/index.js
@@ -1,9 +1,12 @@
 const Section = require('./Section');
 const BasePlate = require('./BasePlate');
 const TensionChordSplice = require('./TensionChordSplice');
+const SplicePlate = require('./SplicePlate');
+
 
 module.exports = {
   Section,
   BasePlate,
-  TensionChordSplice
+  TensionChordSplice,
+  SplicePlate
 }
